fda76f2d640072cd390b23391f89bf62e84c3e3a
Matus <m_juhasz@utb.cz>
2022-12-10 18:20:30 +0100

    Add project files.

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,54 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+
+namespace MyApp.ViewModel
+{
+    public partial class MainViewModel : ObservableObject
+    {
+
+        [ObservableProperty]
+        ObservableCollection<string> items;
+
+        public MainViewModel()
+        {
+            Items = new ObservableCollection<string>();
+        }
+
+        [ObservableProperty]
+        string text;
+
+        [RelayCommand]
+        void Add()
+        {
+            if (string.IsNullOrWhiteSpace(Text)) { return; }
+            Items.Add(Text);
+            Text = string.Empty;
+        }
+
+        [RelayCommand]
+        void Delete(string s)
+        {
+            if (Items.Contains(s))
+            {
+                Items.Remove(s);
+            }
+        }
+    }
+}
+
+
+
+//    public string Text
+//    {
+//        get => text;
+//        set
+//        {
+//            text = value;
+//            OnPropertyChagned(nameof(Text));
+//        } 
+//    }
+
+//    public event PropertyChangedEventHandler PropertyChanged;
+//    void OnPropertyChagned(string name)=>
+//        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
